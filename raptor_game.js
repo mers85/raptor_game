dc6aa995d4476a7f2fe2e7ab623eda91ef305c0b
Mayra Rodriguez <mers85@gmail.com>
2018-07-09 05:45:51 +0200

    life's and img. game over working

--- raptor_game.js
+++ raptor_game.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
   jumping = false;
-  raptor = $(".raptor");
-  raptor.css({top: '563px', left: '12px' });
+  raptor = $(".raptor").show();
+  $raptorPosition = raptor.css({top: '563px', left: '12px' });
 
   newCactus();
-  cactusInterval = setInterval(moveCactus, 3000)
+  cactusInterval = setInterval(moveCactus, 2000);
   $lives = 5;
-
   $score = 500;
 
   $( document ).keydown(function(e) {
-
     if (e.which == 38){
       if (jumping === true){
         return false;
@@ -22,25 +20,17 @@ $(document).ready(function() {
     }else if (e.which == 37){
       raptorGoBack();
     }
-
   });
 
   var jumpHeight = 200;
   function jump() {
     jumping = true;
     y = parseInt(raptor.css('top'));
-    $(raptor).stop().animate({'top':parseInt(y)-jumpHeight+"px"},100,function(){
-      $(raptor).stop().animate({'top': y +"px"}, 800, function(){
+    $(raptor).stop().animate({'top':parseInt(y) - jumpHeight + "px"},100,function(){
+      $(raptor).stop().animate({'top': y +"px"}, 500, function(){
         jumping = false;
       });
     });
-  }
-
-  function updateLives(){
-    for (var i = 0; i < $lives; i++){
-      var life = $("<img id='lives' src='img/lives_img.gif'>");
-      $(".livesUbuntu p").append(life);
-    }
   }
 
   function raptorGoForward(){
@@ -54,9 +44,15 @@ $(document).ready(function() {
     var left = parseInt(raptor.css('left'));
     raptor.css('left', (left - speed)+ "px");
   }
-
 });
 
+function raptorDies(){
+    sangre = $(".raptor_dies");
+    var raptorPleft = parseInt(raptor.css('left'));
+    var raptorPtop =  parseInt(raptor.css('top'));
+    sangre.css({top: raptorPtop + "px", left: raptorPleft + "px"}).show();
+    raptor.hide();
+}
 
 function moveCactus(){
   $(".cactus").each(function(i, obj){
@@ -73,11 +69,10 @@ function newCactus() {
   var cacPosition = cac.css( {top: '563px', left: '2200px' });
   $("body").append(cacPosition);
   var checkCollision = setInterval(function(){collision(cac)}, 500);
-  var checkInterval = setInterval(function(){ checkCactus(checkCollision, cac, checkInterval); }, 500)
+  var checkInterval = setInterval(function(){ checkCactus(checkCollision, cac, checkInterval);}, 500)
 }
 
 function checkCactus(checkCollision, cac, checkInterval){
-
   if (cac.position().left <= 12){
     cac.remove();
     clearInterval(checkCollision);
@@ -85,18 +80,20 @@ function checkCactus(checkCollision, cac, checkInterval){
   }
 
   if (collision(cac)){
-    $lives -= 1
+    $lives -= 1;
     console.log($lives);
     clearInterval(checkInterval);
     clearInterval(checkCollision);
   }
-
-  if ($lives === 0){
-    clearInterval(cactusInterval);
-    console.log('Game Over');
-  }
-
   sumScore(cac);
+  updateLives();
+  if ($lives === 0){
+    clearInterval(checkCollision);
+    clearInterval(cactusInterval);
+    clearInterval(checkInterval);
+    console.log('Game Over');
+    raptorDies();
+  }
 }
 
 function sumScore(cac){
@@ -108,15 +105,20 @@ function sumScore(cac){
   $('#score').text($score);
 }
 
-
+function updateLives(){
+  $(".livesUbuntu p").remove();
+  $(".livesUbuntu").append("<p><span style='top: -5px;'>Lives:</span></p>")
+  for (var i = 0; i < $lives; i++){
+    var life = $("<img style='margin-left:3px; margin-top:-8px;' id='lives' src='img/lives_img.gif'>");
+    $(".livesUbuntu p").append(life);
+  }
+}
 
 function collision(cactus){
   var cactus = cactus.get(0).getBoundingClientRect();
   var raptor = $(".raptor").get(0).getBoundingClientRect();
-
-  if ( (raptor.left <= cactus.left) && ((raptor.right >= cactus.left)) && (raptor.bottom >= cactus.top) ){
+  if ( (raptor.right >= cactus.left) && (raptor.left <= cactus.left) && (raptor.bottom >= cactus.top) ){
     console.log('collision');
     return true;
   }
-
 }
